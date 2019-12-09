218e4e7e98e1f615d7ab7537f343b0583329099b
Siarhei Lunski <loonskai@gmail.com>
2019-12-09 10:56:38 +0300

    Update deploy settings

---
+++ scripts/clean.js
@@ -0,0 +1,11 @@
+const fs = require('fs');
+const path = require('path');
+
+const folders = ['../out'];
+
+folders.forEach(folder => {
+  const dir = path.join(__dirname, folder);
+  if (fs.existsSync(dir)) {
+    fs.rmdirSync(dir, { recursive: true });
+  }
+})
---
+++ scripts/version.js
@@ -0,0 +1,20 @@
+const fs = require('fs');
+
+const v = process.env.npm_package_version;
+const CHANGELOG = 'CHANGELOG.md';
+
+fs.readFile(CHANGELOG, (error, data) => {
+  const stringData = data.toString('utf8');
+  const updated = stringData.replace(/## \[Unreleased\](?!\s*## )/, `## [Unreleased]\n\n## [${v}]`);
+
+  if (updated !== stringData) {
+    console.log(`CHANGELOG: [Unreleased] updated with [${v}]`);
+  }
+
+  fs.writeFile(CHANGELOG, updated, err => {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+  });
+});
